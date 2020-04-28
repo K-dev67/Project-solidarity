802f3ef62f6e8813b9c20aa9ba984f4f60e44e58
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-28 21:43:17 +0200

    export methode and const LOGIN
    login -> homePage connected

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -8,10 +8,19 @@ export const SYNC_MAIL = 'actions/SYNC_MAIL';
 export const SYNC_PASSWORD = 'actions/SYNC_PASSWORD';
 export const SYNC_PASSWORD_CONFIRMATION = 'actions/SYNC_PASSWORD_CONFIRMATION';
 
-// == Page signUp => je submit mon formulaire
+export const RESET = 'actions/RESET';
+
+// == page signUp => je submit mon formulaire
 // == enter -> login Page
 export const SET_FORM = 'actions/SET_FORM';
 export const ENTER_LOGIN_PAGE = 'actions/ENTER_LOGIN_PAGE';
 export const signup = (history) => ({ type: SET_FORM, history });
 export const enterLoginPage = (history) => ({ type: ENTER_LOGIN_PAGE, history });
+
+// == page login => je sub mon login
+// == enter -> homePage même si on peut y accéder dans login (la nav va changer etc)
+export const LOGIN = 'actions/LOGIN';
+export const ENTER_HOME_PAGE = 'actions/ENTER_HOME_PAGE';
+export const login = (history) => ({ type: LOGIN, history });
+export const enterHomePage = (history) => ({ type: ENTER_HOME_PAGE, history });
 
