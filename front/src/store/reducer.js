4446db042fe172a7b86fdfdc0f82f387c61d1aa5
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-28 15:13:51 +0200

    reducer ok with signUp form
    - import all actions
    - add firstname, lastname etc .. in initial state
    - add action to SyncInput from the form

--- front/src/store/reducer.js
+++ front/src/store/reducer.js
@@ -1,10 +1,21 @@
 import {
   SET_INPUT_NAV,
+  SYNC_FIRSTNAME,
+  SYNC_LASTNAME,
+  SYNC_MAIL,
+  SYNC_PASSWORD,
+  SYNC_PASSWORD_CONFIRMATION,
+  // SET_FORM,
 } from './actions';
 
 const initialState = {
   counter: 0,
   inputNav: '',
+  firstname: '',
+  lastname: '',
+  mail: '',
+  password: '',
+  passwordConfirmation: '',
 };
 
 
@@ -14,6 +25,36 @@ export default (state = initialState, action = {}) => {
       return {
         ...state,
         inputNav: action.payload,
+      };
+    }
+    case SYNC_FIRSTNAME: {
+      return {
+        ...state,
+        firstname: action.firstname,
+      };
+    }
+    case SYNC_LASTNAME: {
+      return {
+        ...state,
+        lastname: action.lastname,
+      };
+    }
+    case SYNC_MAIL: {
+      return {
+        ...state,
+        mail: action.mail,
+      };
+    }
+    case SYNC_PASSWORD: {
+      return {
+        ...state,
+        password: action.password,
+      };
+    }
+    case SYNC_PASSWORD_CONFIRMATION: {
+      return {
+        ...state,
+        passwordConfirmation: action.passwordConfirmation,
       };
     }
     default: {
