09ff25ca98e755e9fc5f561678f5c8c25b4d9c81
SamyC-Dev <cosamy2@gmail.com>
2020-04-29 01:58:29 +0200

    component TeacherPresentation add content with few css

--- front/src/components/HomePage/TeacherPresentation.js
+++ front/src/components/HomePage/TeacherPresentation.js
@@ -1,15 +1,38 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // == style
 import './styles.scss';
 
-const TeacherPresentation = () => {
 
+const TeacherPresentation = () => {
+    const profs = fauxProf.map((prof) => {
+        return (
+            <div className="OneCard">
+                <img src={prof.avatar}></img>
+                <h4>{prof.name}</h4>
+                <p>{prof.matter}</p>
+            </div>
+        )
+    })
     return (
-        <div>
-            teacher presentation
+        <div className="TeacherPresentation_main">
+            <h2>Apprenez ou proposez un cours en ligne</h2>
+            <div className="TeacherPresentation_card">
+                {profs}
+            </div>
+            <Link to="/lesprofs">
+                <button className="button AssetsPresentation_button" type="button">Voir la liste des profs</button>
+            </Link>
         </div>
     );
 };
+
+//Fausse données
+let fauxProf = [
+    { name: "Anthony", avatar: "https://www.nicepng.com/png/detail/804-8049853_med-boukrima-specialist-webmaster-php-e-commerce-web.png", matter: "Physique" },
+    { name: "Kevin", avatar: "https://www.nicepng.com/png/detail/804-8049853_med-boukrima-specialist-webmaster-php-e-commerce-web.png", matter: "Math" },
+    { name: "Samy", avatar: "https://www.nicepng.com/png/detail/804-8049853_med-boukrima-specialist-webmaster-php-e-commerce-web.png", matter: "Anglais" },
+    { name: "oclock", avatar: "https://www.nicepng.com/png/detail/804-8049853_med-boukrima-specialist-webmaster-php-e-commerce-web.png", matter: "Informatique" },
+];
 
 export default TeacherPresentation;
