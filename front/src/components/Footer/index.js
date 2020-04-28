24b38b5b0b35acf7802c8310fc0985becf4cceb6
SamyC-Dev <cosamy2@gmail.com>
2020-04-28 17:04:48 +0200

    footer create ok + create all components in HomePage ( presentation, teacher presentation, Lesson presentation, How It Works Presentation, Users Reviews Presentation

---
+++ front/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// == style
+import './styles.css';
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            Contact - Mention
+        </div>
+    );
+};
+
+export default Footer;
---
+++ front/src/components/HomePage/HowItWorksPresentation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// == style
+import './styles.scss';
+
+const HowItWorksPresentation = () => {
+
+    return (
+        <div>
+            How It Works Presentation
+        </div>
+    );
+};
+
+export default HowItWorksPresentation;
