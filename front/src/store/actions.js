c09c47cb2c43cee6d8b08c69a8e72537b8884e89
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-28 15:08:16 +0200

    add actions for form in actions.js

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -1,1 +1,9 @@
+// == input de la navBar
 export const SET_INPUT_NAV = 'actions/SET_INPUT_NAV';
+// == sync userForm in signUp
+export const SYNC_USERNAME = 'actions/SYNC_USERNAME';
+export const SYNC_FIRSTNAME = 'actions/SYNC_FIRSTNAME';
+export const SYNC_LASTNAME = 'actions/SYNC_LASTNAME';
+export const SYNC_MAIL = 'actions/SYNC_MAIL';
+export const SYNC_PASSWORD = 'actions/SYNC_PASSWORD';
+export const SYNC_PASSWORD_CONFIRMATION = 'actions/SYNC_PASSWORD_CONFIRMATION';
