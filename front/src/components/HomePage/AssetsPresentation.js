e12677a561abd8d47d5422e89cb2dc51613dfd3d
SamyC-Dev <cosamy2@gmail.com>
2020-04-29 00:46:22 +0200

    component AssetsPresentation add content with a few css OK

--- front/src/components/HomePage/AssetsPresentation.js
+++ front/src/components/HomePage/AssetsPresentation.js
@@ -7,7 +7,21 @@ const AssetsPresentation = () => {
 
     return (
         <div>
-            Assets Presentation
+            <div className="AssetsPresentation_main">
+                <div className="AssetsPresentation_card">
+                    <h3>Confort</h3>
+                    <p>Des cours depuis le confort de votre domicile, profitez de tous nos cours...</p>
+                </div>
+                <div className="AssetsPresentation_card">
+                    <h3>Salle de cours interactives</h3>
+                    <p>Salle de cours facile a utliser et complete</p>
+                </div>
+                <div className="AssetsPresentation_card">
+                    <h3>Benevolat</h3>
+                    <p>Aussi bien bien assisté a des cours ou en donner, tout est gratuit...</p>
+                </div>
+            </div>
+            <button className="button AssetsPresentation_button" type="button">Voir la liste des cours</button>
         </div>
     );
 };
