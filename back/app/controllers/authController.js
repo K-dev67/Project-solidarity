9223d831b016cf56763219128a3f3151d33260df
Storck Kevin <storck.kevin67@gmail.com>
2020-04-29 16:15:12 +0200

    DELETE 1 Console.log

--- back/app/controllers/authController.js
+++ back/app/controllers/authController.js
@@ -102,7 +102,6 @@ const authController = {
                     res.send(error);
                 }
                 const user = data.rows[0];
-                console.log(user.password);
                 if (!user) {
                     return res.send("Cet email n'existe pas");
                 }
