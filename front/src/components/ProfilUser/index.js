c65311d30690ea29f7542203e773bf6cb59334f2
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-29 21:55:05 +0200

    add profil Component

---
+++ front/src/components/ProfilUser/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// == style
+import './styles.scss';
+
+const ProfilUser = () => {
+    console.log('ProfilUser')
+    return (
+        <div className="profilUser">
+            My profil
+        </div>
+    );
+};
+
+export default ProfilUser;
