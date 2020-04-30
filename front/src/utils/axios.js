f5f55e849a9f46c43d3ed56ec9129864bf4c8792
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-30 16:49:11 +0200

    axios request

---
+++ front/src/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import store from '../store/index';
+
+// == actions
+import { SET_TEACHERS } from '../store/actions';
+
+const teachersRequest = 'http://localhost:8888/teacherList';
+
+const getTeachers = (url = teachersRequest) => {
+  const promise = axios.get(
+    url,
+  );
+  promise.then((res) => {
+    const teachers = res.data;
+    console.log('teachers', teachers);
+    store.dispatch({ type: SET_TEACHERS, payload: teachers });
+  });
+};
+
+export default getTeachers;
