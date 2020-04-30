6f293004bb5a4ae73fd691dc8fc37683a859d19a
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-04-30 16:47:33 +0200

    clean APP

--- front/src/components/HomePage/Presentation.js
+++ front/src/components/HomePage/Presentation.js
@@ -3,19 +3,16 @@ import React from 'react';
 // == style
 import './styles.scss';
 
-const Presentation = () => {
-
-    return (
-        <div className="container_presentation">
-            <div className="container_presentation_main">
-                <h1>Cours Gratuit en Live</h1>
-                <p>Bienvenue sur le projet Solidarity, ce site est basé sur le partage des connaissances, chacun peut enseigner ou tout simplement assister a un cour qu'il apprecie. Enjoy ;-)</p>
-            </div>
-            <div className="container_presentation_image">
-                <img src={image}></img>
-            </div>
-        </div>
-    );
-};
-const image = "https://www.canalvie.com/polopoly_fs/1.11719774.1586371724!/image/limiter.jpg_gen/derivatives/cvlandscape_670_377/limiter.jpg"
+const Presentation = () => (
+  <div className="container_presentation">
+    <div className="container_presentation_main">
+      <h1>Cours Gratuit en Live</h1>
+      <p>Bienvenue sur le projet Solidarity, ce site est basé sur le partage des connaissances, chacun peut enseigner ou tout simplement assister a un cour qu'il apprecie. Enjoy ;-)</p>
+    </div>
+    <div className="container_presentation_image">
+      <img src={image} />
+    </div>
+  </div>
+);
+const image = 'https://www.canalvie.com/polopoly_fs/1.11719774.1586371724!/image/limiter.jpg_gen/derivatives/cvlandscape_670_377/limiter.jpg';
 export default Presentation;
--- front/src/components/HomePage/TopLessonsPresentation.js
+++ front/src/components/HomePage/TopLessonsPresentation.js
@@ -4,37 +4,51 @@ import React from 'react';
 import './styles.scss';
 
 const TopLessonsPresentation = () => {
-    const TopCours = fauxCours.map((cour) => {
-        return (
-            <div key={cour.id} className="TopLessonsPresentation_Card">
-                <img src={cour.teacher_avatar}></img>
-                <h4>{cour.teacher_id}</h4>
-                <p>{cour.description} </p>
-            </div>
-        )
-    })
-    return (
-        <div className="TopLessonsContainer">
-            <h2>Cours les mieux notés</h2>
-            <div className="TopLessonsPresentation_container">
-                <div className="TopLessonsPresentation_container_card"></div>
-                {TopCours}
-            </div>
-        </div>
-    );
+  const TopCours = fauxCours.map((cour) => (
+    <div key={cour.id} className="TopLessonsPresentation_Card">
+      <img src={cour.teacher_avatar} />
+      <h4>{cour.teacher_id}</h4>
+      <p>{cour.description} </p>
+    </div>
+  ));
+  return (
+    <div className="TopLessonsContainer">
+      <h2>Cours les mieux notés</h2>
+      <div className="TopLessonsPresentation_container">
+        <div className="TopLessonsPresentation_container_card" />
+        {TopCours}
+      </div>
+    </div>
+  );
 };
 
-//"title", "description","level", "teacher_id", "plannified", "status"
+// "title", "description","level", "teacher_id", "plannified", "status"
 
 const fauxCours = [
-    { id: 1, title: "Javascript", description: "cours complet Javascript ES6", teacher_id: "Anthony", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSyalOicjSEpYodgWxemjYhuBZ3GeSuEGbRZzVWJoWXVPbXqPHH&usqp=CAU" },
-    { id: 2, title: "React", description: "cours complet sur React / Redux", teacher_id: "Jean-Denis", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU" },
-    { id: 3, title: "Data/SQL", description: "cours Data en javascript", teacher_id: "Kevin", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKbRH4Jh9B0D6CXyNByqqfuoKXwcbYsSkHoFwp6fsHabi1cygm&usqp=CAU" },
-    { id: 4, title: "Les divisions", description: "Petit cours sur les divisions mathematique", teacher_id: "Samy", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT8wvd_Id1I40Xzuse5dBaTpypBpfFAHp0y5ruHFnNlOKhkCEic&usqp=CAU" },
-    { id: 5, title: "NodeJS", description: "cours technologie Node JS", teacher_id: "Simon", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU" },
-    { id: 6, title: "Francais", description: "cours revisions francais grammaire", teacher_id: "Alexi", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU" },
-    { id: 7, title: "Html/Css/Javascript", description: "Revision les bases du Web", teacher_id: "Tony", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRThBSUZ1GFF_lB9loNha1PS-wPB4mOTfij9AfhgnPuVBqm5pAP&usqp=CAU" },
-    { id: 8, title: "Troll", description: "Comment troller tout le monde ", teacher_id: "Chris", teacher_avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU" },
-]
+  {
+    id: 1, title: 'Javascript', description: 'cours complet Javascript ES6', teacher_id: 'Anthony', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSyalOicjSEpYodgWxemjYhuBZ3GeSuEGbRZzVWJoWXVPbXqPHH&usqp=CAU',
+  },
+  {
+    id: 2, title: 'React', description: 'cours complet sur React / Redux', teacher_id: 'Jean-Denis', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU',
+  },
+  {
+    id: 3, title: 'Data/SQL', description: 'cours Data en javascript', teacher_id: 'Kevin', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKbRH4Jh9B0D6CXyNByqqfuoKXwcbYsSkHoFwp6fsHabi1cygm&usqp=CAU',
+  },
+  {
+    id: 4, title: 'Les divisions', description: 'Petit cours sur les divisions mathematique', teacher_id: 'Samy', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT8wvd_Id1I40Xzuse5dBaTpypBpfFAHp0y5ruHFnNlOKhkCEic&usqp=CAU',
+  },
+  {
+    id: 5, title: 'NodeJS', description: 'cours technologie Node JS', teacher_id: 'Simon', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU',
+  },
+  {
+    id: 6, title: 'Francais', description: 'cours revisions francais grammaire', teacher_id: 'Alexi', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU',
+  },
+  {
+    id: 7, title: 'Html/Css/Javascript', description: 'Revision les bases du Web', teacher_id: 'Tony', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRThBSUZ1GFF_lB9loNha1PS-wPB4mOTfij9AfhgnPuVBqm5pAP&usqp=CAU',
+  },
+  {
+    id: 8, title: 'Troll', description: 'Comment troller tout le monde ', teacher_id: 'Chris', teacher_avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU',
+  },
+];
 
 export default TopLessonsPresentation;
