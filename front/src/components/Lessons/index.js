8be370431bda624fa18d80e41cd6685a12c27829
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-02 00:26:15 +0200

    lesson Page done :+1:
    - todo: css

--- front/src/components/Lessons/index.js
+++ front/src/components/Lessons/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+// == import component semantic
+import { Card, Image, Icon } from 'semantic-ui-react';
+
+// react Moment
+import Moment from 'react-moment';
+import 'moment/locale/fr';
+
 // == style
 import './styles.scss';
 
@@ -8,9 +15,34 @@ import './styles.scss';
 const Lessons = () => {
   const lessons = useSelector((state) => state.lessons);
   console.log('lessons', lessons);
+  // todo gerer la photo du prof avec lesson.teacher_id
+  const lessonsJSX = lessons.map((lesson) => (
+    <Card>
+      <Image src="{avataree}" wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{lesson.title}</Card.Header>
+        <Card.Meta>
+          <span className="date">leçon crée il y a <Moment locale="fr" fromNow ago>{lesson.created_at}</Moment> </span>
+        </Card.Meta>
+        <Card.Description>
+          {lesson.description}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          {lesson.level}
+        </a>
+      </Card.Content>
+    </Card>
+  ));
   return (
     <div className="lessons">
-      Lesson
+      <Card.Group
+        itemsPerRow={3}
+        stackable
+      >{lessonsJSX}
+      </Card.Group>
     </div>
   );
 };
