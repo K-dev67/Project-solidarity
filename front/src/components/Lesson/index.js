e1295bac52e7d14f38d37e047f39daf6fb4f7325
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-02 17:01:03 +0200

    room ok :muscle:

--- front/src/components/Lesson/index.js
+++ front/src/components/Lesson/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// react Moment
+import Moment from 'react-moment';
+import 'moment/locale/fr';
+
 // == style
 import './styles.scss';
 
@@ -8,8 +12,16 @@ import './styles.scss';
 const Lesson = ({ lesson }) => {
   console.log(lesson);
   return (
-    <div className="Lesson">
-      Cockpit numero
+    <div className="lesson">
+      <span># Cockpit numero {lesson.id}</span>
+      <h2>{lesson.title}</h2>
+      <div>Salon crée le
+        <Moment format="D MMM YYYY" withTitle>
+          {lesson.created_at}
+        </Moment>
+      </div>
+      <div>niveau : {lesson.level}</div>
+      <div>{lesson.description}</div>
     </div>
   );
 };
