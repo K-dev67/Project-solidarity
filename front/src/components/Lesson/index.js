c7c3c49bd78caa31be8218c18f298546e9793ca3
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-07 00:09:08 +0200

    fix css
    - on modal update lesson
    - modal add lesson
    - add icon in chatRoom

--- front/src/components/Lesson/index.js
+++ front/src/components/Lesson/index.js
@@ -9,7 +9,7 @@ import Moment from 'react-moment';
 import 'moment/locale/fr';
 
 // == semantic
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 // == action
 import { DELETE_LESSON, ADD_CATEGORY_ON_LESSON } from '../../store/actions';
@@ -93,9 +93,10 @@ const Lesson = ({ lesson }) => {
 
   return (
     <div className="room">
+      <Icon size="large" name="chevron circle left" />
       <div className="room--description">
         <span className="room-number"># Cockpit numero {lesson.id}</span>
-        <h2 className="room-title">titre : {lesson.title}</h2>
+        <h2 className="room-title">{lesson.title}</h2>
         <LabelCategory lessonId={lesson.id} teacherId={lesson.teacher_id} />
         <div className="room-created-date">Salon crée le : <Moment format="D MMM YYYY" withTitle>{lesson.created_at}</Moment>
         </div>
