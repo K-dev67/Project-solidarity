cde0dca62998964890855c90e05cad60c08c97c4
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-07 16:32:08 +0200

    clean APP

--- front/src/components/HomePage/index.js
+++ front/src/components/HomePage/index.js
@@ -7,9 +7,6 @@ import TopLessonsPresentation from './TopLessonsPresentation';
 import HowItWorksPresentation from './HowItWorksPresentation';
 import UsersReviewsPresentation from './UsersReviewsPresentation';
 import AssetsPresentation from './AssetsPresentation';
-import Socket from './socket';
-// import components
-// import Presentation2 from './Presentation2';
 
 // == style
 import './styles.scss';
@@ -17,13 +14,11 @@ import './styles.scss';
 const HomePage = () => (
   <div className="homePage">
     <Presentation />
-    {/* <Presentation2 /> */}
     <AssetsPresentation />
     <TeacherPresentation />
     <TopLessonsPresentation />
     <HowItWorksPresentation />
     <UsersReviewsPresentation />
-    <Socket />
   </div>
 );
 
