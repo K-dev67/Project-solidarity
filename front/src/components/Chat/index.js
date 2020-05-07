c9db8063049329c6b3591f9be8642ceed26d22bc
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-07 16:28:45 +0200

    test chatRoom ok!

---
+++ front/src/components/Chat/index.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { sendMessage, syncMessage } from 'src/store/actions';
+
+// == utils/axios
+import getMessages from '../../utils/getMessages';
+
+// == style
+import './styles.scss';
+
+
+const Chat = () => {
+  const dispatch = useDispatch();
+
+  const currentMessage = useSelector((state) => state.message);
+  const { messages, user } = useSelector((state) => state);
+  useEffect(getMessages, []);
+  if (messages === undefined) return null;
+  const messageJSX = messages.map((message) => <li>{message.firstname} : {message.content}</li>);
+
+  return (
+    <div className="chat-main">
+      <p className="chat-nickname">{user.firstname}</p>
+      <ul className="chat-messages">{messageJSX}</ul>
+      <form
+        className="chat-send-message"
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          dispatch(sendMessage());
+        }}
+      >
+        <input
+          type="text"
+          className="chat-send-message_input"
+          placeholder="Votre message"
+          value={currentMessage}
+          onChange={(evt) => {
+            dispatch(syncMessage(evt.target.value));
+          }}
+        />
+        <button
+          type="submit"
+          className="chat-send-message_button"
+        >
+          Envoyer
+        </button>
+      </form>
+    </div>
+  );
+};
+
+
+export default Chat;
