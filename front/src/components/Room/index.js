672a2a60e61c91de0d3acaaf09dcc1f211cd10e1
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-13 21:01:56 +0200

    update all atyle for a room :+1:

--- front/src/components/Room/index.js
+++ front/src/components/Room/index.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+
 import axios from 'axios';
 
 
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 // == semantic
 import { Button } from 'semantic-ui-react';
 import { API_URL } from '../../utils/constante';
 
 // == action
-import { LEAVE_ROOM, SET_LESSON_BY_ID } from '../../store/actions';
+import { SET_LESSON_BY_ID } from '../../store/actions';
 
 // == import utils/fetchInBdd
-
 import getCategories from '../../utils/getCategories';
 
 // component
 import RoomDescription from './RoomDescription';
-import LabelCategory from './LabelCategory';
+
 import Chat from '../Chat';
-import RoomUsers from './RoomUsers';
-import UpdateTeacherComponent from './UpdateTeacherComponent';
+
 import Video from '../VideoLive';
 
 // == style
 import './styles.scss';
 import store from '../../store';
 
+import background from '../../assets/img/Pattern_fond.png';
+
 const Room = ({ lesson }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   useEffect(
     () => {
       axios
@@ -47,41 +47,40 @@ const Room = ({ lesson }) => {
   let buttonSentence = 'Suivre le live';
   if (userId === lesson.teacher_id) buttonSentence = 'Lancer le live';
   const [askLive, setAskLive] = useState(false);
+  const [buttonDisplay, setButtonDisplay] = useState('block');
   let videoLiveComponent = '';
   if (askLive === true) {
     videoLiveComponent = <Video lesson={lesson} />;
   }
+  // == background image from live
+  const divStyleStream = {
+    backgroundImage: `url(${background})`,
+  };
+  // == display none ou block if live is on or off
+  const divStyleButton = {
+    display: `${buttonDisplay}`,
+  };
 
   return (
     <>
-      <header className="header-room">
-        <Link
-          fluid
-          to="/lessons/"
-          onClick={() => {
-            dispatch({ type: LEAVE_ROOM });
-          }}
-        >
-          <Button
-            color="red"
-            fluid
-          >Quitter le cours
-          </Button>
-        </Link>
-        <LabelCategory lessonId={lesson.id} teacherId={lesson.teacher_id} />
-      </header>
       <div className="room">
-        <UpdateTeacherComponent lesson={lesson} />
-        <Button
-          color="red"
-          onClick={() => setAskLive(true)}
-        >{buttonSentence}
-        </Button>
-        {videoLiveComponent}
-        <RoomDescription lesson={lesson} />
+        <div className="container-live-description">
+          <div className="live-stream" style={divStyleStream}>
+            {videoLiveComponent}
+            <Button
+              style={divStyleButton}
+              color="red"
+              onClick={() => {
+                setAskLive(true);
+                setButtonDisplay('none');
+              }}
+            >{buttonSentence}
+            </Button>
+          </div>
+          <RoomDescription lesson={lesson} />
+        </div>
         <div className="tchat">
           <Chat lessonId={lesson.id} />
-          <RoomUsers />
         </div>
       </div>
     </>
