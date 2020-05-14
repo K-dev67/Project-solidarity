a9ca3a2bd97d5ed37e1f02fb9124d9aedf8636df
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-14 15:54:52 +0200

    add positive message on ask lesson modal

---
+++ front/src/components/AskLessons/MessagePositive.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Message } from 'semantic-ui-react';
+
+const MessagePositive = () => {
+  const { messagePositif } = useSelector((state) => state);
+
+  console.log('message');
+  return (
+    <Message positive hidden={!messagePositif}>
+      <Message.Header>Votre demande de cours a bien été ajouté</Message.Header>
+    </Message>
+  );
+};
+
+
+export default MessagePositive;
