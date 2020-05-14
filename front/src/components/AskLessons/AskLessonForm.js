904e9b700a5b793da87c7c1026155085ef4c45ed
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-14 15:54:03 +0200

    add ask lesson form

---
+++ front/src/components/AskLessons/AskLessonForm.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+// == react hook form
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+
+const AskLessonForm = () => {
+  useEffect();
+  return (
+    <div>Hello</div>
+  );
+};
+export default AskLessonForm;
