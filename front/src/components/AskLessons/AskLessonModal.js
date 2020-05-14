0405dd88f25e2027bafe1fe4d45f913087e8a8c2
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-14 15:54:13 +0200

    ad ask lesson modal

---
+++ front/src/components/AskLessons/AskLessonModal.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Button, Modal,
+} from 'semantic-ui-react';
+import { MESSAGE_POSTIF_FALSE } from '../../store/actions';
+
+// == component
+import AskLessonForm from './AskLessonForm';
+import MessagePositive from './MessagePositive';
+// == background
+import background from '../../assets/img/Pattern_fond.png';
+
+const AskLessonModal = () => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch({ type: MESSAGE_POSTIF_FALSE });
+  };
+  const contentStyle = {
+    backgroundImage: `url(${background})`,
+  };
+  return (
+    <Modal trigger={<Button onClick={handleClick} fluid>Faire une demande de cours</Button>}>
+      <Modal.Header style={contentStyle}>Faire une demande de cours</Modal.Header>
+      <Modal.Content>
+        <MessagePositive />
+        {/* <Image wrapped size="medium" src="/images/avatar/large/rachel.png" /> */}
+        <Modal.Description>
+          {/* <Header>Default Profile Image</Header> */}
+          {/* <AskLessonForm /> */}
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default AskLessonModal;
