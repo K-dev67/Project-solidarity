9e8cf234b40ca46f101985842037603e5722a7b2
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-16 10:11:32 +0200

    add picture on /lessons

---
+++ front/src/components/Lessons/ImageCategory.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// == semanthic
+import {
+  Image,
+} from 'semantic-ui-react';
+
+// == Import les images
+import geographie from '../../assets/img/category/Geographie.png';
+import informatique from '../../assets/img/category/Informatique.png';
+import marketing from '../../assets/img/category/Marketing.png';
+import maths from '../../assets/img/category/Maths.png';
+import physique from '../../assets/img/category/Physique.png';
+
+const ImageCategory = ({ picture }) => (
+  <>
+    {/* <Image src={`/src/assets/img/category/${lesson.picture}.png`} size="medium" bordered /> */}
+    {/* <Image src="./src/assets/img/category/Geographie.png" size="medium" bordered /> */}
+    <Image src={maths} size="medium" bordered />
+  </>
+);
+
+export default ImageCategory;
