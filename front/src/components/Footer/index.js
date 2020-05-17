8e3cb2bf2ecb3466f979c633a346e9aef6dc2965
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-17 16:46:53 +0200

    updated footer

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 // == style
 import './styles.scss';
 
@@ -10,9 +11,9 @@ import { Icon } from 'semantic-ui-react';
 
 const Footer = () => (
   <div className="footer">
-    <div className="social-media"><a><Icon name="facebook official" size="large" /></a></div>
+    <div className="social-media"><a href="https://www.facebook.com/" target="_blank"><Icon name="facebook official" size="large" /></a></div>
     <div className="assets-footer">Contact - Mention</div>
-    <div className="social-media2"><a><Icon name="instagram" size="large" /></a></div>
+    <div className="social-media2"><a href="https://www.instagram.com/" target="_blank"><Icon name="instagram" size="large" /></a></div>
   </div>
 );
 
