545a09a5be8e589daa933af43d805941bbafe950
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-17 16:40:18 +0200

    Update UsersReviewsPresentation.js

--- front/src/components/HomePage/UsersReviewsPresentation.js
+++ front/src/components/HomePage/UsersReviewsPresentation.js
@@ -4,9 +4,21 @@ import React from 'react';
 import './styles.scss';
 
 const UsersReviewsPresentation = () => {
-  const lesAvis = AvisUser.map((Oneavis) => (
+  const userReviews = [
+    {
+      id: 1, avis: "'J'ai vraiment apprecié ce site. Très ergonomique, facile et pratique'", avatar: 'https://dreambuilders.dk/wp-content/uploads/2015/03/myAvatar-61.png', name: 'Lionel', status: "Professeur d'anglais",
+    },
+    {
+      id: 2, avis: "'J'ai participé à un cour c'est vraiment génial merci beaucoup'", avatar: 'https://i.pinimg.com/originals/4b/5d/19/4b5d1954fbb5b6bad18f0ac25c4ab3c3.png', name: 'Fabienne', status: 'Eleve',
+    },
+    {
+      id: 3, avis: '"Très pratique pour donner des cours ou en recevoir."', avatar: 'https://mybluerobot.com/wp-content/uploads/2015/04/myAvatar-29.png', name: 'Tony', status: 'Professeur math',
+    },
+  ];
+
+  const lesAvis = userReviews.map((Oneavis) => (
     <div key={Oneavis.id} className="UsersReviewsPresentation_card">
-      <img src={Oneavis.avatar} />
+      <img src={Oneavis.avatar} alt="avatar_user-review" />
       <p>{Oneavis.avis}</p>
       <h4>{Oneavis.name}</h4>
       <p>{Oneavis.status}</p>
@@ -22,17 +34,5 @@ const UsersReviewsPresentation = () => {
   );
 };
 
-
-const AvisUser = [
-  {
-    id: 1, avis: "j'ai vraiment apprecié ce site. Très ergonomique, facile et pratique", avatar: 'https://dreambuilders.dk/wp-content/uploads/2015/03/myAvatar-61.png', name: 'Lionel', status: "Professeur d'anglais",
-  },
-  {
-    id: 2, avis: "j'ai participé à un cour c'est vraiment génial merci beaucoup", avatar: 'https://i.pinimg.com/originals/4b/5d/19/4b5d1954fbb5b6bad18f0ac25c4ab3c3.png', name: 'Fabienne', status: 'Eleve',
-  },
-  {
-    id: 3, avis: 'Très pratique pour donner des cours ou en recevoir.', avatar: 'https://mybluerobot.com/wp-content/uploads/2015/04/myAvatar-29.png', name: 'Tony', status: 'Professeur math',
-  },
-];
 
 export default UsersReviewsPresentation;
