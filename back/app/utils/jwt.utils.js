ae61f6d86c6b32d57986daa44b964449335ae54f
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-18 09:40:00 +0200

    JWT token :+1:

---
+++ back/app/utils/jwt.utils.js
@@ -0,0 +1,19 @@
+// imports
+const jwt = require('jsonwebtoken');
+
+
+const JWT_SIGN_SECRET = process.env.SECRET_SIGN_TOKEN;
+
+// export
+module.exports = {
+    generateTokenForUser: function(userData) {
+        return jwt.sign({
+            userId: userData.id,
+            // mettre le rôle à terme
+        },
+        JWT_SIGN_SECRET,
+        {
+            expiresIn: '3h'
+        })
+    }
+}
