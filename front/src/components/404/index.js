8e3a5d845dbeb85fca168130c9591cb8cf508ca3
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-18 20:21:27 +0200

    add 404 responsive :+1:

---
+++ front/src/components/404/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// == style
+import './styles.scss';
+
+// == import 404 picture
+import quatre from '../../assets/img/404.jpg';
+
+
+const Quatre = () => (
+  <div className="quatre">
+    <img src={quatre} alt="404_picture" />
+  </div>
+);
+
+export default Quatre;
