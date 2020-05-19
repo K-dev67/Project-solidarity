62cc082525073043e394e472a00c1fbd689f700e
Storck Kevin <storck.kevin67@gmail.com>
2020-05-19 10:38:33 +0200

    Mege for antho

--- front/src/components/Lessons/ImageCategory.js
+++ front/src/components/Lessons/ImageCategory.js
@@ -15,13 +15,13 @@ import anglais from '../../assets/img/category/anglais.jpg';
 import biologie from '../../assets/img/category/biologie.jpg';
 import economie from '../../assets/img/category/economie.jpg';
 import espagnol from '../../assets/img/category/espagnol.jpg';
-import geographie from '../../assets/img/category/geographie.png';
+import geographie from '../../assets/img/category/Geographie.png';
 import histoire from '../../assets/img/category/histoire.jpg';
 import informatique from '../../assets/img/category/Informatique.png';
-import marketing from '../../assets/img/category/marketing.png';
-import mathematiques from '../../assets/img/category/mathematiques.png';
+import marketing from '../../assets/img/category/Marketing.png';
+import mathematiques from '../../assets/img/category/Mathematiques.png';
 import philosophie from '../../assets/img/category/philosophie.jpg';
-import physique from '../../assets/img/category/physique.png';
+import physique from '../../assets/img/category/Physique.png';
 import sciences_sociales from '../../assets/img/category/sciences_sociales.jpg';
 import yoga from '../../assets/img/category/yoga.jpg';
 
