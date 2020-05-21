f06220c8f99149870ea58d78053e5971ffe2f602
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-21 19:43:24 +0200

    Update AssetsPresentation.js

--- front/src/components/HomePage/AssetsPresentation.js
+++ front/src/components/HomePage/AssetsPresentation.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 // == style
 import './styles.scss';
 
 const AssetsPresentation = () => (
   <div className="container-assets-presentation">
-    <div className="AssetsPresentation_main">
-      <div className="AssetsPresentation_card">
+    <div className="assetsPresentation_main">
+      <div className="assetsPresentation_card">
+        <Icon name="leaf" size="big" />
         <h3>Confort</h3>
         <p>Profitez du confort de votre domicile pour suivre vos cours</p>
       </div>
-      <div className="AssetsPresentation_card">
+      <div className="assetsPresentation_card">
+        <Icon name="desktop" size="big" />
         <h3>Salles de cours interactives</h3>
         <p>Salles de cours facile à utliser et complète</p>
       </div>
-      <div className="AssetsPresentation_card">
+      <div className="assetsPresentation_card">
+        <Icon name="gift" size="big" />
         <h3>Bénevolat</h3>
         <p>Assister à des cours ou en proposer, tout est gratuit...</p>
       </div>
@@ -22,7 +26,7 @@ const AssetsPresentation = () => (
 
     <div className="button-assets-presentation">
       <Link to="/lessons">
-        <button className="button AssetsPresentation_button" type="button">Voir la liste des cours</button>
+        <button className="button assetsPresentation_button" type="button">Voir la liste des cours</button>
       </Link>
     </div>
   </div>
