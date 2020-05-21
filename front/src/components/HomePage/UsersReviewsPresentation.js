ece9c4e87869fdc85295a4432567a99323ce8abe
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-05-21 19:44:22 +0200

    Update UsersReviewsPresentation.js

--- front/src/components/HomePage/UsersReviewsPresentation.js
+++ front/src/components/HomePage/UsersReviewsPresentation.js
@@ -6,13 +6,10 @@ import './styles.scss';
 const UsersReviewsPresentation = () => {
   const userReviews = [
     {
-      id: 1, avis: "'J'ai vraiment apprecié ce site. Très ergonomique, facile et pratique'", avatar: 'https://dreambuilders.dk/wp-content/uploads/2015/03/myAvatar-61.png', name: 'Lionel', status: "Professeur d'anglais",
+      id: 1, avis: "'J'ai vraiment apprecié ce site. Très ergonomique, facile et pratique'", avatar: 'https://dreambuilders.dk/wp-content/uploads/2015/03/myAvatar-61.png', name: 'Anthony', status: 'Scrum master',
     },
     {
-      id: 2, avis: "'J'ai participé à un cour c'est vraiment génial merci beaucoup'", avatar: 'https://i.pinimg.com/originals/4b/5d/19/4b5d1954fbb5b6bad18f0ac25c4ab3c3.png', name: 'Fabienne', status: 'Eleve',
-    },
-    {
-      id: 3, avis: '"Très pratique pour donner des cours ou en recevoir."', avatar: 'https://mybluerobot.com/wp-content/uploads/2015/04/myAvatar-29.png', name: 'Tony', status: 'Professeur math',
+      id: 2, avis: "'J'ai participé à un cour c'est vraiment génial merci beaucoup'", avatar: 'https://i.pinimg.com/originals/4b/5d/19/4b5d1954fbb5b6bad18f0ac25c4ab3c3.png', name: 'Kevin', status: 'Lead dev back',
     },
   ];
 
@@ -26,7 +23,7 @@ const UsersReviewsPresentation = () => {
   ));
   return (
     <div className="UsersReviewsPresentation_container">
-      <h2>Ce que disent nos utilisateurs</h2>
+      <h2>Notre equipe</h2>
       <div className="UsersReviewsPresentation_container_card">
         {lesAvis}
       </div>
